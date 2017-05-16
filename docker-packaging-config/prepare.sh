c5c54f5da873c6e9dbba4b3c0959c137d567a07c
Didier Hoarau <didier.hoarau.dev@gmail.com>
2017-05-17 06:03:21 +0800

    Fix packaging

--- docker-packaging-config/prepare.sh
+++ docker-packaging-config/prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
-# Registrator
 npm install
-cp -R $PROJECT_DIR/node_modules $PROJECT_PACKAGE_FILES_DIR
-cp -R $PROJECT_DIR/src $PROJECT_PACKAGE_FILES_DIR
-cp -R $PROJECT_DIR/node_modules $PROJECT_PACKAGE_FILES_DIR
-rm -f $PROJECT_PACKAGE_FILES_DIR/src/config/config.override.json
+
+rm -fr $PACKAGING_FILES
+mkdir -p $PACKAGING_FILES
+
+cp -R $PROJECT_DIR/node_modules $PACKAGING_FILES
+cp -R $PROJECT_DIR/src $PACKAGING_FILES
