1f22f6caf2acce1d029f74338a72ea8899237528
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-04-17 09:23:46 +0800

    Fix docker packaging

--- docker-packaging-config/prepare.sh
+++ docker-packaging-config/prepare.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 npm install
 
+npm run build
+
 cp -R $PROJECT_DIR/node_modules $PACKAGING_FILES
-cp -R $PROJECT_DIR/src-proxy $PACKAGING_FILES
-cp -R $PROJECT_DIR/src-watcher $PACKAGING_FILES
+cp -R $PROJECT_DIR/dist-proxy $PACKAGING_FILES
+cp -R $PROJECT_DIR/dist-watcher $PACKAGING_FILES
